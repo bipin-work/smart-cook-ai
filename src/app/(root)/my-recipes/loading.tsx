0c48168c583d9bb00e9879bd622c6de1cbfb9093
Bipin Shrestha <bipinshrestha.work@gmail.com>
2026-03-30 13:59:35 +0545

    Add loader to my-recipe page

---
+++ src/app/(root)/my-recipes/loading.tsx
@@ -0,0 +1,13 @@
+export default function RecipeListLoader() {
+  return (
+    <div className="p-8">
+      <div className="max-w-6xl mx-auto space-y-6">
+        <div className="space-y-2 animate-pulse">
+          <div className="h-6 bg-gray-300 rounded w-3/4" />
+          <div className="h-6 bg-gray-300 rounded w-full" />
+          <div className="h-6 bg-gray-300 rounded w-1/2" />
+        </div>
+      </div>
+    </div>
+  );
+}
