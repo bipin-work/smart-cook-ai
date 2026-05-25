d5f8aefad7ce26a8f202b814b7f349b706b42516
Bipin Shrestha <bipinshrestha.work@gmail.com>
2026-05-25 20:01:36 +0545

    Sign out fix

--- src/components/shared/sign-out-button.tsx
+++ src/components/shared/sign-out-button.tsx
@@ -5,13 +5,15 @@ import { LogOut } from "lucide-react";
 
 const SignOutButton = () => {
   return (
-    <Button
-      variant="ghost"
-      onClick={signOutUser}
-      className="w-full flex flex-start gap-2 px-4 py-2.5 rounded-lg text-sm font-medium text-destructive hover:bg-destructive/10 transition-colors tracking-[-0.1504px]"
-    >
-      <LogOut /> Sign Out
-    </Button>
+    <form action={signOutUser}>
+      <Button
+        type="submit"
+        variant="ghost"
+        className="w-full flex flex-start gap-2 px-4 py-2.5 rounded-lg text-sm font-medium text-destructive hover:bg-destructive/10 transition-colors tracking-[-0.1504px]"
+      >
+        <LogOut /> Sign Out
+      </Button>
+    </form>
   );
 };
 
